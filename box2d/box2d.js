98b46035812694067b3d28d7b0fb5efeccf230ce
Marc Bracher <m.bracher@mac.com>
2012-12-23 20:21:29 +0100

    debug draw now drawing in canvas coordinates.

--- box2d/box2d.js
+++ box2d/box2d.js
@@ -6018,6 +6018,7 @@ Box2D.postDefs = [];
             for (b = this.m_bodyList;
                  b; b = b.m_next) {
                 xf = b.m_xf;
+//debugger;
                 for (f = b.GetFixtureList();
                      f; f = f.m_next) {
                     s = f.GetShape();
@@ -10794,29 +10795,35 @@ Box2D.postDefs = [];
     b2DebugDraw.prototype.DrawPolygon = function (vertices, vertexCount, color) {
         if (!vertexCount) return;
         var s = this.m_ctx;
+        var wc = s.canvas.width/2;
+        var hc = s.canvas.height/2;
         var drawScale = this.m_drawScale;
         s.beginPath();
         s.strokeStyle = this._color(color.color, this.m_alpha);
-        s.moveTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+        s.moveTo(-(wc - vertices[0].x * drawScale), hc - vertices[0].y * drawScale);
         for (var i = 1; i < vertexCount; i++) {
-            s.lineTo(vertices[i].x * drawScale, vertices[i].y * drawScale);
+            s.lineTo(-(wc - vertices[i].x * drawScale), hc - vertices[i].y * drawScale);
         }
-        s.lineTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+        s.lineTo(-(wc - vertices[0].x * drawScale), hc - vertices[0].y * drawScale);
         s.closePath();
         s.stroke();
     };
     b2DebugDraw.prototype.DrawSolidPolygon = function (vertices, vertexCount, color) {
         if (!vertexCount) return;
         var s = this.m_ctx;
+        var wc = s.canvas.width/2;
+        var hc = s.canvas.height/2;
+        //debugger;
         var drawScale = this.m_drawScale;
         s.beginPath();
+
         s.strokeStyle = this._color(color.color, this.m_alpha);
         s.fillStyle = this._color(color.color, this.m_fillAlpha);
-        s.moveTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+        s.moveTo(-(wc - vertices[0].x * drawScale), hc - vertices[0].y * drawScale);
         for (var i = 1; i < vertexCount; i++) {
-            s.lineTo(vertices[i].x * drawScale, vertices[i].y * drawScale);
+            s.lineTo(-(wc - vertices[i].x * drawScale), hc - vertices[i].y * drawScale);
         }
-        s.lineTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+        s.lineTo(-(wc - vertices[0].x * drawScale), hc - vertices[0].y * drawScale);
         s.closePath();
         s.fill();
         s.stroke();
@@ -10824,26 +10831,32 @@ Box2D.postDefs = [];
     b2DebugDraw.prototype.DrawCircle = function (center, radius, color) {
         if (!radius) return;
         var s = this.m_ctx;
+        var wc = s.canvas.width/2 ;
+        var hc = s.canvas.height/2;
         var drawScale = this.m_drawScale;
         s.beginPath();
         s.strokeStyle = this._color(color.color, this.m_alpha);
-        s.arc(center.x * drawScale, center.y * drawScale, radius * drawScale, 0, Math.PI * 2, true);
+        s.arc(-(wc - center.x * drawScale, hc - center.y * drawScale), radius * drawScale, 0, Math.PI * 2, true);
         s.closePath();
         s.stroke();
     };
     b2DebugDraw.prototype.DrawSolidCircle = function (center, radius, axis, color) {
         if (!radius) return;
-        var s = this.m_ctx,
-            drawScale = this.m_drawScale,
-            cx = center.x * drawScale,
-            cy = center.y * drawScale;
+        var s = this.m_ctx;
+        var wc = s.canvas.width / 2;
+        var hc = s.canvas.height/2;
+
+        var drawScale = this.m_drawScale,
+            cx = wc - center.x * drawScale,
+            cy = hc - center.y * drawScale;
+
         s.moveTo(0, 0);
         s.beginPath();
         s.strokeStyle = this._color(color.color, this.m_alpha);
         s.fillStyle = this._color(color.color, this.m_fillAlpha);
-        s.arc(cx, cy, radius * drawScale, 0, Math.PI * 2, true);
-        s.moveTo(cx, cy);
-        s.lineTo((center.x + axis.x * radius) * drawScale, (center.y + axis.y * radius) * drawScale);
+        s.arc(-cx, cy, radius * drawScale, 0, Math.PI * 2, true);
+        s.moveTo(-cx, cy);
+        //s.lineTo(wc - (center.x + axis.x * radius) * drawScale, hc + (center.y + axis.y * radius) * drawScale);
         s.closePath();
         s.fill();
         s.stroke();
@@ -10851,24 +10864,28 @@ Box2D.postDefs = [];
     b2DebugDraw.prototype.DrawSegment = function (p1, p2, color) {
         var s = this.m_ctx,
             drawScale = this.m_drawScale;
+        var wc = s.canvas.width/2;
+        var hc = s.canvas.height/2;
         s.strokeStyle = this._color(color.color, this.m_alpha);
         s.beginPath();
-        s.moveTo(p1.x * drawScale, p1.y * drawScale);
-        s.lineTo(p2.x * drawScale, p2.y * drawScale);
+        s.moveTo(-(wc - p1.x * drawScale), hc - p1.y * drawScale);
+        s.lineTo(-(wc - p2.x * drawScale), hc - p2.y * drawScale);
         s.closePath();
         s.stroke();
     };
     b2DebugDraw.prototype.DrawTransform = function (xf) {
         var s = this.m_ctx,
             drawScale = this.m_drawScale;
+        var wc = s.canvas.width/2;
+        var hc = s.canvas.height/2;
         s.beginPath();
         s.strokeStyle = this._color(0xff0000, this.m_alpha);
-        s.moveTo(xf.position.x * drawScale, xf.position.y * drawScale);
-        s.lineTo((xf.position.x + this.m_xformScale * xf.R.col1.x) * drawScale, (xf.position.y + this.m_xformScale * xf.R.col1.y) * drawScale);
+        s.moveTo(-(wc - xf.position.x * drawScale), hc - xf.position.y * drawScale);
+        s.lineTo(-(wc - (xf.position.x + this.m_xformScale * xf.R.col1.x) * drawScale), hc - (xf.position.y + this.m_xformScale * xf.R.col1.y) * drawScale);
 
         s.strokeStyle = this._color(0xff00, this.m_alpha);
-        s.moveTo(xf.position.x * drawScale, xf.position.y * drawScale);
-        s.lineTo((xf.position.x + this.m_xformScale * xf.R.col2.x) * drawScale, (xf.position.y + this.m_xformScale * xf.R.col2.y) * drawScale);
+        s.moveTo(-(wc - xf.position.x * drawScale), hc - xf.position.y * drawScale);
+        s.lineTo(-(wc - (xf.position.x + this.m_xformScale * xf.R.col2.x) * drawScale), hc - (xf.position.y + this.m_xformScale * xf.R.col2.y) * drawScale);
         s.closePath();
         s.stroke();
     };
